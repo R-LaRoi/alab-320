b926850bc04214f27dd257d776763e6957249722
R. LaRoi <are@raewhoo.com>
2024-08-08 19:35:21 -0400

    added checkboxes

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,18 @@ interface Todo {
   completed: boolean;
 }
 
+interface AddTodosProps {
+  title: string
+}
+
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>([]); 
+  const [inputText, setInputText] = useState<string | undefined>();
+  const [userTodos, setUserTodos] = useState<string[]>([]);
+
+ const [isChecked, setIsChecked] = useState<boolean[]> ([]);
+
+
 
   async function getTodos() {
     try {
@@ -20,36 +30,87 @@ function App() {
       console.error('Error', error);
     }
   }
-
   useEffect(() => {
     getTodos();
   }, []);
 
+    function newTodo(event: React.FormEvent<HTMLFormElement>) {
+      event.preventDefault();
+      let userText = new FormData(event.currentTarget);
+      let value = userText.get('inputText') 
+        if (value !== null && typeof value === 'string') {
+      setInputText(value);
+      setUserTodos((prevUserTodos) => [...prevUserTodos, value as string]);
+
+    } else {
+      setInputText('');
+    }
+
+
+
+  }
+ 
+
+  const handleCheckbox = (index: number) => {
+    setIsChecked((prev) => {
+      const updatedChecked = [...prev];
+      updatedChecked[index] = !updatedChecked[index];
+      return updatedChecked;
+    });
+  };
+
+
+
   return (
     <>
       <h1>Task List</h1>
+      <form onSubmit={newTodo}>
+      <span>
+      <input type="text" placeholder="add todo"  name="inputText"/>
+     <input type="submit" value="add"  /></span>
+    </form>
       <ul>
+      {userTodos.map((todo, index) => (
+        <li key={index}>
+          <input
+            type="checkbox"
+            name="done"
+            checked={isChecked[index]}
+            onChange={() => handleCheckbox(index)}
+          />
+          {todo}
+          {isChecked[index] && (
+            <input
+              type="button"
+              value="delete"
+              onClick={() => console.log(`Delete button clicked for item ${index}`)}
+            />
+          )}
+        </li>
+      ))}
+  
+    
      {todos.slice(0, 10).map((todo) => (
-    <li key={todo.id}>{todo.title}</li>
+     <li key={todo.id}> <input
+            type="checkbox"
+            name="done"
+            checked={isChecked[todo.id]}
+            onChange={() => handleCheckbox(todo.id)}
+          />{todo.title} 
+       {isChecked[todo.id] && (
+            <input
+              type="button"
+              value="delete"
+              onClick={() => console.log(`Delete  item ${todo.id}`)}
+            />
+          )}</li>
+
+
+
   ))}
       </ul>
 
 <section>
-
-   <form>
-    <span><input type="text" />
-    <input type="button" value="add" /></span>
-    <br/>
-    <span><input type="checkbox" name="Create Mockup" id="" />
-    <label htmlFor="Create Mockup">Create Mockup</label></span>    <br/>
-<span>
-  <input type="checkbox" name="Create Static Layout" id="" />
-    <label htmlFor="Create Static Layout">Create Static Layout</label>
-   </span>
-     <br/>
-<span> <input type="checkbox" name="" id="" /><input type="text" name="" id="" placeholder="add interactivity"/><input type="button" value="save" /></span>
-  
-   </form>
 </section>
 
 
